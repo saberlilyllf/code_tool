02063870587d98bee6b9a96fb3ce05561749e009
saberlilyllf <cll2035@163.com>
2024-07-18 21:08:02 +0800

    add hazard poiner implement

--- kdtree_test.cpp
+++ kdtree_test.cpp
@@ -9,6 +9,8 @@
 #include <kdtree_define.h>
 #include <print_tree.hpp>
 #include <lock_free_stack.h>
+// #include <experimental/atomic>  
+
 int main() {
     std::vector<Point> point_v {{30,40}, {5,25}, {10,12}, {70,70}, {50,30}, {35,45}};
     TreeNode* root = nullptr;
@@ -49,10 +51,10 @@ int main() {
     std::atomic<Point*> node_ptr;
     // std::shared_ptr<Point> node_ptr = std::make_shared<Point>(30,40);
     bool is_ptr_lock_free = std::atomic_is_lock_free(&node_ptr);
-
+    
     // bool is_ptr_lock_free = std::atomic<std::shared_ptr<Point>>()::is_lock_free();
 
-    std::cout << " is ptr lock free " << is_ptr_lock_free;
+    std::cout << " is ptr lock free " << is_ptr_lock_free << "\n";
     return 0;
 }
 
--- lock_free_stack.h
+++ lock_free_stack.h
@@ -4,6 +4,9 @@
 #include <algorithm>
 #include <cmath>
 #include <atomic>
+#include <thread>
+#include <exception>
+#include <functional>
 
 template<typename T>
 class LockFreeStackLeakNode {
@@ -104,5 +107,134 @@ void LockFreeStackList74<T>::TryReclaim(Node* old_head) {
         ChainPendingNode(old_head);
         --threads_in_pop;
     }
-
 }
+
+uint32_t const max_hazard_pointers = 100;
+struct HazardPointer {
+    std::atomic<std::thread::id> id;
+    std::atomic<void*> pointer;
+};
+HazardPointer hazard_pointers[max_hazard_pointers];
+
+class HpOwner {
+    HazardPointer* hp;
+public:
+    HpOwner(const HpOwner&) = delete;
+    HpOwner() : hp (nullptr) {
+        for (uint32_t i = 0; i < max_hazard_pointers; ++i) {
+            std::thread::id old_id;
+            if (hazard_pointers[i].id.compare_exchange_strong(old_id, std::this_thread::get_id())) {
+                hp = &hazard_pointers[i];
+                break;
+            }
+        }
+        if (!hp) {
+            throw std::runtime_error("No Hazard Pointers avaliable");
+        }
+    }
+    ~HpOwner() {
+        hp->pointer.store(nullptr);
+        hp->id.store(std::thread::id());
+    }
+    std::atomic<void*>& GetPointer() {
+        return hp->pointer;
+    }
+};
+
+std::atomic<void*>& GetHazardPointerForCurrentThread() {
+    thread_local static HpOwner hazard;
+    return hazard.GetPointer();
+}
+
+bool OutstandingHazardPointersFor(void* p) {
+    for (uint32_t i = 0; i < max_hazard_pointers; ++i) {
+        if (hazard_pointers[i].pointer.load() == p) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<typename T>
+void do_delete(void* p) {
+    delete static_cast<T*>(p);
+}
+
+struct DataToReclaim {
+    void* data;
+    std::function<void(void*)> deleter;
+    DataToReclaim* next;
+    template<typename T>
+    DataToReclaim(T* p) : data (p), deleter(&do_delete<T>), next(nullptr) {}
+    ~DataToReclaim() {
+        deleter(data);
+    }
+};
+
+std::atomic<DataToReclaim*> nodes_to_reclaim;
+void add_to_reclaim_list(DataToReclaim* node) {
+    if (!node) {
+        return;
+    }
+    node->next = nodes_to_reclaim.load();
+    while (!nodes_to_reclaim.compare_exchange_weak(node->next, node));
+}
+
+void DeleteNodesWithNoHazards() {
+    DataToReclaim* current = nodes_to_reclaim.exchange(nullptr);
+    while (current) {
+        DataToReclaim* next = current->next;
+        if (!OutstandingHazardPointersFor(current->data)) {
+            delete current;
+        } else {
+            add_to_reclaim_list(current);
+        }
+        current = next;
+    }
+}
+
+template<typename T>
+void ReclaimLater(T* data) {
+    add_to_reclaim_list(new DataToReclaim(data));
+}
+template<typename T>
+class LockFreeStackList77 {
+    struct Node {
+        std::shared_ptr<T> data;
+        Node* next;
+        Node(const T& data_node) : data(std::make_shared<T>(data_node)) {}
+
+    };
+    std::atomic<Node*> head;
+
+public:
+    void Push(const T& data) {
+        Node* new_node = new Node(data);
+        new_node->next = head.load();
+        while (!head.compare_exchange_weak(new_node->next, new_node));
+    }
+    std::shared_ptr<T> pop() {
+        std::atomic<void*>& hp = GetHazardPointerForCurrentThread();
+        Node* old_head = head.load();
+        do {
+            Node* temp = nullptr;
+            do {
+                temp = old_head;
+                hp.store(old_head);
+                old_head = head.load();
+            } while (old_head != temp);
+        } while(old_head && !head.compare_exchange_weak(old_head, old_head->next));
+        hp.store(nullptr);
+        std::shared_ptr<T> res;
+        if(old_head) {
+            res.swap(old_head->data);
+            if (OutstandingHazardPointersFor(old_head)) {
+                ReclaimLater(old_head);
+            } else {
+                delete old_head;
+            }
+            DeleteNodesWithNoHazards();
+        }
+        return res;
+    }
+};
